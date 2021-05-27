df0274813ce487377c3e3c4254e8b0fe1d226660
Balvinder Kumar <84867597+balvinder-bunny@users.noreply.github.com>
2021-05-27 20:31:08 +0530

    Update q2.py

--- q2.py
+++ q2.py
@@ -12,17 +12,25 @@ Dict2: (this is a key value pair of user id and exam score)
    “2” : 60
    “3” : 70
 }
-These are just sample data assume there are hundreds of users"""
+These are just sample data assume there are hundreds of users
+Write a function in python in python, which will return maximum i.e function should return dictionary like
+{
+  “3” : 70
+}
+"""
 
 
 def dict_1_2(n):
     l=[str(i) for i in range(1,n+1)]
     dict1={}
     dict2={}
+    dict3={}
     for i in l:
         dict1[i]=input("please enter the name of student")
     for i in dict1:
         dict2[i]=int(input("enter the marks of the student"))
-    print("{}\n{}".format(dict1,dict2))
+    ans=max(dict2.items())
+    dict3[ans[0]]=ans[1]
+    return dict3
 n=int(input("enter the length of the dictionary"))
-dict_1_2(n)
+print(dict_1_2(n))
